f311ffae526d492e49b315128424e9b7cfc078ba
srfarias <ericfari15@gmail.com>
2016-06-07 14:26:48 -0300

    Bring up turbo

--- turbo.mk
+++ turbo.mk
@@ -1,8 +1,8 @@
 $(call inherit-product, device/motorola/otus/full_otus.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common TURBO stuff.
+$(call inherit-product, vendor/turbo/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_otus
+PRODUCT_NAME := turbo_otus
 PRODUCT_RELEASE_NAME := MOTO E 3G
